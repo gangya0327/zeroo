70c6bba4523225410709af94acb59d0087b0822d
gangya0327 <king9999427@163.com>
2018-03-20 17:37:46 +0800

    bitvue-march

---
+++ bitVue/march-vue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p v-once>hello {{world}}</p>
+      <p v-text="'hello' + world"></p>
+      <p>{{`hello ${world}`}}</p>
+      <p v-text="`hello ${world}`"></p>
+      <button @click="world='worlds'">改变world</button>
+      <p v-html="htmlss">change here</p>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      world: 'uufaf',
+      htmlss: `<span style="color: red">改变了！</span>`
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
