e13dc929768a1f827fc975957cda452f6b7c449d
gangya0327 <king9999427@163.com>
2018-03-21 17:41:33 +0800

    some vue

--- bitVue/march-vue/src/App.vue
+++ bitVue/march-vue/src/App.vue
@@ -8,18 +8,83 @@
       <p v-text="`hello ${world}`"></p>
       <button @click="world='worlds'">改变world</button>
       <p v-html="htmlss">change here</p>
+      <a :href='hrefss'>百度一下</a>
+      <a v-bind:href='hrefss'>百度一下</a>
+      <div id="method1">
+        <p>{{ count < 0 ? ' + ' : '-' }} {{ count  }}</p>
+        <button @click='count ++'>增加</button>
+        <button @click='count --'>减少</button>
+      </div>
+      <div id="method2">
+        <input type="text" v-model="msg">
+        <p>{{msg.split('').reverse().join('')}}</p>
+      </div>
     </div>
-    <router-view/>
+    <div>
+      <mybtn @click.native='buttonClick'></mybtn>
+    </div>
+    <div>
+      <p v-for='tel in telephone'>{{tel | newtel}}</p>
+    </div>
+    <div>
+      <p v-for='(url, index) in urlList'>
+        <a :href='url.url | getquery(url.name, url.age)'>{{url.url}}</a>
+      </p>
+    </div>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
+import mybtn from './components/btn.vue'
 export default {
   name: 'App',
   data () {
     return {
       world: 'uufaf',
-      htmlss: `<span style="color: red">改变了！</span>`
+      htmlss: `<span style="color: red">有样式的data</span>`,
+      hrefss: 'http://www.baidu.com',
+      count: 0,
+      msg: 'hello world',
+      telephone: [
+        10000001,
+        10000002,
+        10000003,
+        10000004,
+        10000005,
+        10000006,
+        10000007,
+      ],
+      urlList: [
+        {url: 'http://www.baidu.com', name: 'pa', age: 30},
+        {url: 'http://www.qq.com', name: 'tc', age: 16},
+      ]
+    }
+  },
+  filters: {
+    newtel(value){
+      if(!value) return ''
+      value = value.toString().substr(4,4)
+      value = '*'.repeat(4) + value
+      let endMember = value.substr(-1,1)
+      if(endMember%2){
+        value = value + ' 中奖'
+      }else{
+        value = value + ' 谢谢'
+      }
+      return value
+    },
+    getquery(value, name, age){
+      if(!value) return ''
+      return `${value}?name=${name}&age=${age}`
+    }
+  },
+  components: {
+    mybtn
+  },
+  methods: {
+    buttonClick(){
+      alert('ok!')
     }
   }
 }
---
+++ bitVue/march-vue/src/components/btn.vue
@@ -0,0 +1,10 @@
+<template>
+  <button>按钮啊</button>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
