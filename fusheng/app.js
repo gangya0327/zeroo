2309751f4cf7e816780dbc0758a89b8526c68852
gangya0327 <king9999427@163.com>
2018-04-13 17:34:09 +0800

    some

--- fusheng/app.js
+++ fusheng/app.js
@@ -33,6 +33,34 @@ App({
       }
     })
   },
+  onLoad: function () {
+    //获取openid  
+    var user = wx.getStorageSync('user') || {};
+    if (!user.openid || (user.expires_in || Date.now()) < (Date.now() + 600)) {//不要在30天后才更换openid-尽量提前10分钟更新  
+      wx.login({
+        success: function (res) {
+          // success  
+          var d = that.globalData.wxData;//这里存储了appid、secret、token串  
+          var l = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + d.appId + '&secret=' + d.appSecret + '&js_code=' + res.code + '&grant_type=authorization_code';
+          wx.request({
+            url: l,
+            data: {},
+            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+            // header: {}, // 设置请求的 header  
+            success: function (res) {
+              var obj = {};
+              obj.openid = res.data.openid;
+              obj.expires_in = Date.now() + res.data.expires_in;
+
+              wx.setStorageSync('user', obj);//存储openid  
+            }
+          });
+        }
+      });
+    } else {
+      console.log(user);
+    }
+  },
   globalData: {
     userInfo: null
   }
--- fusheng/pages/detail/detail.js
+++ fusheng/pages/detail/detail.js
@@ -1,66 +1,23 @@
 // pages/detail/detail.js
+var wxParse = require('../../wxParse/wxParse.js')
+/**
+* WxParse.wxParse(bindName , type, data, target,imagePadding)
+* 1.bindName绑定的数据名(必填)
+* 2.type可以为html或者md(必填)
+* 3.data为传入的具体数据(必填)
+* 4.target为Page对象,一般为this(必填)
+* 5.imagePadding为当图片自适应是左右的单一padding(默认为0,可选)
+*/
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    info: {
+      id: 1,
+      img: '../../image/li1.jpg',
+      title: '春语入绘 匠心结缘 《阴阳师》创作大赛集结再启！',
+      time: '2018-04-12',
+      content1: '携手走过每日逢魔，一起挑战八歧大蛇，共斗麒麟。同在平安京，阴阳师的经历各有不同，在才华横溢的阴阳师眼中的平安京是怎样呢？',
+      content2: '多领域创作大赛—大触觉醒·阴阳师创作大赛，再次集结！春语入绘，匠心结缘，大触觉醒·阴阳师创作大赛，和你一起赏尽芳菲！',
+      content3: '大触觉醒系列比赛中，绮妆魅服cosplay大赛、百绘罗衣式神皮肤设计大赛和礼乐和鸣音乐大赛已抢先一步和大家见面;更多精彩大赛纷至沓来，大触在此觉醒！'
+    }
   }
 })
