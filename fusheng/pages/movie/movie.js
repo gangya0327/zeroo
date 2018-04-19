dcd08ee5dc3f8a72517c2cb7d68c9408bc04e8e5
gangya0327 <king9999427@163.com>
2018-04-19 17:47:42 +0800

    dd

--- fusheng/pages/movie/movie.js
+++ fusheng/pages/movie/movie.js
@@ -1,25 +1,31 @@
-var api_url = "https://t.yushu.im/v2/movie/top250"
-var api_url1 = "https://t.yushu.im/v2/book/1220562"
-var api_url2 = "https://api.douban.com/v2/movie/in_theaters"
+var api_url = "https://douban.uieee.com/v2/movie/top250"
 
 Page({
   data: {
+    title: '加载中',
     movies: []
   },
-  onLoad: function(){
+  onLoad: function () {
+    var that = this
     wx.showToast({
       title: '加载中...',
       icon: "loading",
       duration: 10000
     })
     wx.request({
-      url: api_url1,
+      url: api_url,
       data: {},
       header: {
-        'content-type': 'application/json' // 默认值
+        'content-type': 'json' // 默认值
       },
-      success: function(res){
-        console.log(res.data)
+      success: function (res) {
+        wx.hideToast()
+        var data = res.data
+        that.setData({
+          title: data.title,
+          movies: data.subjects
+        })
+        //console.log(res.data)
       }
     })
   }
--- fusheng/pages/movieInfo/movieInfo.js
+++ fusheng/pages/movieInfo/movieInfo.js
@@ -1,66 +1,27 @@
-// pages/movieInfo/movieInfo.js
+var api_url = 'https://douban.uieee.com/v2/movie/subject/'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    movie: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  onLoad: function (param) {
+    var that = this
+    wx.showLoading({
+      title: '正在读取信息',
+    })
+    wx.request({
+      url: api_url + param.id,
+      data: {},
+      header: {
+        'content-type': 'json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        wx.hideLoading()
+        that.setData({
+          movie: res.data
+        })
+      }
+    })
   }
 })
