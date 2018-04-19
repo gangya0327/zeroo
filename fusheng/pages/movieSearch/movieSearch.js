f0f9775ef1311854cd5bbde3a8df9f2b8684742b
raven <king9999427@163.com>
2018-04-20 04:20:47 +0800

    updown

--- fusheng/pages/movieSearch/movieSearch.js
+++ fusheng/pages/movieSearch/movieSearch.js
@@ -1,66 +1,27 @@
-// pages/movieSearch/movieSearch.js
+var api_url = 'https://douban.uieee.com/v2/movie/search'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    movies: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  searchMovie: function (e) {
+    if (!e.detail.value) return
+    wx.showLoading({
+      title: '正在查找...'
+    })
+    var that = this
+    wx.request({
+      url: api_url + '?q=' + e.detail.value,
+      data: {},
+      header: {
+        'content-type': 'json' // 默认值
+      },
+      success: function (res) {
+        wx.hideLoading()
+        that.setData({
+          movies: res.data.subjects
+        })
+      }
+    })
   }
 })
